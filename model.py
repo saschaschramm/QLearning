65111a1ee3451e117807d8c72b07abeace1a0e90
saschaschramm <sascha.schramm@gmail.com>
2018-03-06 19:49:37 +0100

    -

---
+++ model.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+class Model():
+    def __init__(self, observation_space, action_space):
+
+        self.inputs = tf.placeholder(shape=[1], dtype=tf.int32)
+        self.q_out = tf.layers.dense(inputs=tf.one_hot(self.inputs, observation_space), units=action_space, activation=None)
+
+        self.predict_action = tf.argmax(self.q_out, 1)
+        self.next_q = tf.placeholder(shape=[1, action_space], dtype=tf.float32)
+
+        loss = tf.reduce_sum(tf.squared_difference(self.next_q, self.q_out))
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+
+        self.optimize = optimizer.minimize(loss)
+        self.session = tf.Session()
+        self.session.run(tf.global_variables_initializer())
+
+    def predict_q(self, observation):
+        actions, q = self.session.run([self.predict_action, self.q_out], feed_dict={self.inputs: [observation]})
+        return actions[0], q
+
+    def predict_q_next(self, observation):
+        return self.session.run(self.q_out, feed_dict={self.inputs: [observation]})
+
+    def train(self, observation, target_q):
+        self.session.run(self.optimize, feed_dict={self.inputs: [observation], self.next_q: target_q})
---
+++ run.py
@@ -0,0 +1,75 @@
+import gym
+import numpy as np
+import random
+import tensorflow as tf
+from model import Model
+
+"""
+1500 0.293
+2000 0.365
+2500 0.372
+3000 0.386
+3500 0.386
+4000 0.431
+4500 0.466
+"""
+
+class ExplorationScheduler:
+    def __init__(self, timesteps, start_prob, end_prob):
+        self.timesteps = timesteps
+        self.start_prob = start_prob
+        self.end_prob = end_prob
+
+    def value(self, t):
+        fraction = min(float(t) / self.timesteps, 1.0)
+        return self.start_prob + fraction * (self.end_prob - self.start_prob)
+
+def main():
+    env = gym.make('FrozenLake-v0')
+    env.seed(0)
+    tf.set_random_seed(0)
+    random.seed(0)
+    np.random.seed(0)
+
+    model = Model(observation_space=16, action_space=4)
+    discount_rate = .99
+    num_episodes = 5000
+    total_rewards = []
+
+    exploration = ExplorationScheduler(timesteps=num_episodes, start_prob=0.1, end_prob=0.02)
+
+    for episode in range(num_episodes):
+        observation = env.reset()
+        total_reward = 0
+
+        while True:
+            action, target_q = model.predict_q(observation)
+            epsilon = exploration.value(episode)
+
+            if np.random.rand(1) < epsilon:
+                action = env.action_space.sample()
+
+            next_observation, reward, done, info = env.step(action)
+
+            if done:
+                target_q[0, action] = reward
+            else:
+                next_q = model.predict_q_next(next_observation)
+                target_q[0, action] = reward + discount_rate * np.max(next_q)
+
+            model.train(observation, target_q)
+
+            total_reward += reward
+            observation = next_observation
+
+            if done == True:
+                break
+
+        if episode % 500 == 0 and episode > 1000:
+            score = sum(total_rewards[-1000:]) / 1000.0
+            print("{} {}".format(episode, score))
+
+        total_rewards.append(total_reward)
+
+if __name__ == "__main__":
+    main()
